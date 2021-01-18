a01aae7ddfd2ed4eee0180485ccb1a89a2cfe4a6
zach6585 <zmines@wesleyan.edu>
2021-01-17 19:54:39 -0500

    commit tree 6446b6d1e20ccdaeb2d9109439120caa03a3cb6b

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+    return sourceArray.map(function(el) {
+        return el*(-1)
+    })
+}
+
+function mapToNoChange(sourceArray) {
+    return sourceArray.map(function(el) {
+        return el
+    })
+}
+
+function mapToDouble(sourceArray) {
+    return sourceArray.map(function(el) {
+        return el*(2)
+    })
+}
+
+function mapToSquare(sourceArray) {
+    return sourceArray.map(function(el) {
+        return el**(2)
+    })
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+    return sourceArray.reduce(function(total, element) {
+        return total + element 
+    }, startingPoint)
+}
+
+function reduceToAllTrue(sourceArray) {
+    return sourceArray.reduce(function(total, element) {
+        if (element) {
+            return true
+        }
+        else {
+            return false 
+        }
+    })
+}
+
+function reduceToAnyTrue(sourceArray) {
+    return sourceArray.reduce(function(total, element) {
+        if (element || total) {
+            return true 
+        } 
+        else {
+            return false 
+        }
+    })
+}
